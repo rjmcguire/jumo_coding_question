6d72a99fb2df2df0f510ebc79b42fe67c667553b
Rory <rjmcguire@gmail.com>
2017-08-21 13:48:19 +0200

    add unittest for CSV line parser

--- source/csv.d
+++ source/csv.d
@@ -44,47 +44,7 @@ struct CSV {
 
 		auto line = byline.front;
 
-		size_t start;
-		bool inString1, inString2;
-		for (size_t i=0; i < line.length; i++) {
-			switch (line[i]) {
-			case ',':
-				front ~= line[start .. i].idup;
-				start = i+1;
-				goto default;
-			case '"':
-				if (inString1) {continue;}
-
-				if (!inString2) {	// opening text field
-					start = i+1;
-				} else {			// closing text field
-					front ~= line[start .. i].idup;
-					do { i++; } while (line[i] == ' ' || line[i] == '\t');
-					start = i+1;
-				}
-				inString2 = !inString2;
-				goto default;
-			case '\'':
-				if (inString2) {continue;}
-
-				if (!inString1) {	// opening text field
-					start = i+1;
-				} else {			// closing text field
-					front ~= line[start .. i].idup;
-					do { i++; } while (line[i] == ' ' || line[i] == '\t');
-					start = i+1;
-				}
-				inString1 = !inString1;
-				goto default;
-			default:
-			}
-			// part of current field in csv
-		}
-		front ~= line[start .. $].idup;
-		writeln("lidne: ", front);
-		if (front[1].length > 20) {
-			assert(0);
-		}
+		front = parseCSV(line);
 	}
 }
 
@@ -92,4 +52,59 @@ auto loadData() {
 	File file;
 	file = stdin;
 	return CSV(file);
+}
+
+
+string[] parseCSV(const char[] line) {
+	string[] ret;
+
+	size_t start;
+	bool inString1, inString2;
+	for (size_t i=0; i < line.length; i++) {
+		switch (line[i]) {
+		case ',':
+			ret ~= line[start .. i].idup;
+			start = i+1;
+			goto default;
+		case '"':
+			if (inString1) {continue;}
+
+			if (!inString2) {	// opening text field
+				start = i+1;
+			} else {			// closing text field
+				ret ~= line[start .. i].idup;
+				do { i++; } while (line[i] == ' ' || line[i] == '\t');
+				start = i+1;
+			}
+			inString2 = !inString2;
+			goto default;
+		case '\'':
+			if (inString2) {continue;}
+
+			if (!inString1) {	// opening text field
+				start = i+1;
+			} else {			// closing text field
+				ret ~= line[start .. i].idup;
+				do { i++; } while (line[i] == ' ' || line[i] == '\t');
+				start = i+1;
+			}
+			inString1 = !inString1;
+			goto default;
+		default:
+		}
+		// part of current field in csv
+	}
+	ret ~= line[start .. $].idup;
+	writeln("lidne: ", ret);
+	return ret;
+}
+
+unittest {
+	auto row = parseCSV("asdf, 1235,'asdgij rh rh\" aisdrhjerihj', \"asdigjdrjhrhj\",123 43 222");
+	assert(row.length == 5, "row length incorrect");
+	assert(row[0] == "asdf", "incorrect value");
+	assert(row[1] == " 1235", "incorrect value");
+	assert(row[2] == "asdgij rh rh\" aisdrhjerihj", "incorrect value");
+	assert(row[3] == "asdigjdrjhrhj", "incorrect value");
+	assert(row[4] == "123 43 222", "incorrect value");
 }

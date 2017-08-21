351be4b4205fbf05a13c8d83acfbecf44d77e05c
Rory <rjmcguire@gmail.com>
2017-08-21 10:31:49 +0200

    some structural direction

--- source/app.d
+++ source/app.d
@@ -1,5 +1,33 @@
 import std.stdio;
 
+auto readCSV(T)(T lines) {
+	auto line = lines.front.idup;
+	lines.popFront;
+	return line;
+}
+
+
+void loadData() {
+	auto lines = stdin.byLine;
+
+	auto header = lines.readCSV();
+	writeln(header);
+
+	foreach (line; lines) {
+		writeln(line);
+	}
+}
+
+/**
+ * Base class for a two dimensional dataset
+ */
+class DataStore {
+	string[] lines;
+	this() {
+		loadData();
+	}
+}
+
 void main() {
-	writeln("Edit source/app.d to start your project.");
+	auto data = new DataStore();
 }
